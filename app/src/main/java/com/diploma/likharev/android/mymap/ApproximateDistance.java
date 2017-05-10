2add37addf2b32d8d0dc743627837c4a92d4ac31
Lirik1992 <ldo1992@list.ru>
2017-05-10 14:10:35 +0300

    Add files via upload

---
+++ app/src/main/java/com/diploma/likharev/android/mymap/ApproximateDistance.java
@@ -0,0 +1,16 @@
+package com.diploma.likharev.android.mymap;
+
+/**
+ * Created by Lirik on 04.05.2017.
+ */
+
+public class ApproximateDistance {
+
+    public String text;
+    public int value;
+
+    public ApproximateDistance(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
---
+++ app/src/main/java/com/diploma/likharev/android/mymap/ApproximateDuration.java
@@ -0,0 +1,16 @@
+package com.diploma.likharev.android.mymap;
+
+/**
+ * Created by Lirik on 04.05.2017.
+ */
+
+public class ApproximateDuration {
+
+    public String text;
+    public int value;
+
+    public ApproximateDuration(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
